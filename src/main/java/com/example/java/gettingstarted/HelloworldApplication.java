645082cc412d515ee2c89982adef6da4278e9413
Jenkins <jenkins@ip-172-31-74-42.ec2.internal>
2019-03-03 16:38:07 +0000

    changed

--- src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
+++ src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloworldApplication {
   @RequestMapping("/")
   public String home() {
-    return "Hello World!-Dev";
+    return "Hello World!";
   }
 
   /**
